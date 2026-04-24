273ca9b346e203f4f467fcceba623a1b6621759a
Ronei.Ferreira <rifla2016@hotmail.com>
2026-04-24 14:41:11 -0300

    atualizei javascript

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ let btnBack = document.querySelector('.back')
 let container = document.querySelector('.container')
 let list = document.querySelector('.container .list')
 let thumb = document.querySelector('.container .thumb')
+
+btnNext.onclick = () => moveItemsOnClick('next')
+btnBack.onclick = () => moveItemsOnClick('back')
+
+
+function moveItemsOnClick(type) {
+    let listItems = document.querySelectorAll('.list .list-item')
+    let thumbItems = document.querySelectorAll('.thumb .thumb-item')
+
+    console.log(listItems)
+    console.log(thumbItems)
+
+    if (type === 'next') {
+        list.appendChild(listItems[0])
+        thumb.appendChild(thumbItems[0])
+       
+    } else {
+         list.insertBefore(listItems[listItems.length - 1], listItems[0])
+        thumb.insertBefore(thumbItems[thumbItems.length - 1], thumbItems[0])
+    }
+}
