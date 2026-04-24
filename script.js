5c59689ce60bb4bb5b70916f499d91fe799df2c5
Ronei.Ferreira <rifla2016@hotmail.com>
2026-04-24 13:01:29 -0300

    criei java script

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnNext = document.querySelector('.next')
+let btnBack = document.querySelector('.back')
+
+let container = document.querySelector('.container')
+let list = document.querySelector('.container .list')
+let thumb = document.querySelector('.container .thumb')
